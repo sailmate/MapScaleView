8c1963b788c2ddc7ce51afb46e2c99038f2a4b80
Olli Jokinen <olli.jokinen@columbiaroad.com>
2018-03-12 17:50:46 +0200

    Replace miles with nautical miles

--- mapscaleview/src/main/java/com/github/pengrad/mapscaleview/MapScaleModel.java
+++ mapscaleview/src/main/java/com/github/pengrad/mapscaleview/MapScaleModel.java
@@ -4,6 +4,7 @@ class MapScaleModel {
 
     private static final double TILE_SIZE_METERS_AT_0_ZOOM = 156543.03;
     private static final double TILE_SIZE_FT_AT_0_ZOOM = 513592.62;
+    private static final double TILE_SIZE_NM_1000TH_AT_0_ZOOM = 845264.74;
 
     private static final int FT_IN_MILE = 5280;
 
@@ -13,6 +14,9 @@ class MapScaleModel {
     private static final int[] FT = {1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000,
             FT_IN_MILE, 2 * FT_IN_MILE, 5 * FT_IN_MILE, 10 * FT_IN_MILE, 20 * FT_IN_MILE, 50 * FT_IN_MILE,
             100 * FT_IN_MILE, 200 * FT_IN_MILE, 500 * FT_IN_MILE, 1000 * FT_IN_MILE, 2000 * FT_IN_MILE};
+
+    private static final int[] NM_1000TH = {1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000,
+            10000, 20000, 50000, 100000, 200000, 500000, 1000000, 2000000};
 
     private final float density;
     private int maxWidth;
@@ -41,8 +45,8 @@ class MapScaleModel {
         double latitude = lastLatitude;
         if (zoom < 0 || Math.abs(latitude) > 90) return null;
 
-        double tileSizeAtZoom0 = meters ? TILE_SIZE_METERS_AT_0_ZOOM : TILE_SIZE_FT_AT_0_ZOOM;
-        int[] distances = meters ? METERS : FT;
+        double tileSizeAtZoom0 = meters ? TILE_SIZE_METERS_AT_0_ZOOM : TILE_SIZE_NM_1000TH_AT_0_ZOOM;
+        int[] distances = meters ? METERS : NM_1000TH;
 
         final double resolution = tileSizeAtZoom0 / density * Math.cos(latitude * Math.PI / 180) / Math.pow(2, zoom);
 
@@ -65,8 +69,7 @@ class MapScaleModel {
             if (distance < 1000) return distance + " m";
             else return distance / 1000 + " km";
         } else {
-            if (distance < FT_IN_MILE) return distance + " ft";
-            else return distance / FT_IN_MILE + " mi";
+            return distance * 1000.0 + " nm";
         }
     }
 }
